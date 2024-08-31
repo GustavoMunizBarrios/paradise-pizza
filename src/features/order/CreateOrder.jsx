d86a7ec7f2faf945a601be490c54d0038295c2cd
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-30 19:52:02 -0600

    Added react-router-dom, created routes, and updated components to use routing

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
+console.log(isValidPhone);
 
 const fakeCart = [
   {
@@ -29,14 +30,15 @@ const fakeCart = [
     totalPrice: 15,
   },
 ];
+console.log(fakeCart);
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  // const cart = fakeCart;
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>{"Ready to order? Let's go!"}</h2>
 
       <form>
         <div>
