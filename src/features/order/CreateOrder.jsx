a9616bed97b411ebb0c014b8a190009497a43d9f
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-02 21:51:37 -0600

    Added useActionData hook and form error handling for phone input in CreateOrder component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -37,6 +37,8 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -55,6 +57,8 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {/*If there is an error then display: "Please give us your correct phone number" */}
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
