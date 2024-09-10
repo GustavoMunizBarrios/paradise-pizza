0cad68dc95f3db017589f232913f8dd53203a2aa
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-09 21:03:43 -0600

    Removed Button import and replaced Button component with DeleteItem component in CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import Button from '../../ui/Button';
+// import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import DeleteItem from './DeleteItem';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-  console.log(pizzaId);
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -13,7 +13,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
