cc591b23001d6059b37c0511de47ecb10f1ee8f3
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-09 20:28:10 -0600

    Extracted total cart quantity and price calculations into separate functions in cartSlice.js and updated CartOverview.jsx to use them

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -42,4 +42,11 @@ export const {
   decreaseItemQuantity,
   clearCart,
 } = cartSlice.actions;
+
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
