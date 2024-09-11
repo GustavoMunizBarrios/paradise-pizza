aab200c284b2b7dedecd4b59c76fbc01d6b82c61
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-10 19:56:26 -0600

    Added useDispatch and fetchAddress from userSlice, and a button to trigger it in CreateOrder component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,11 +4,12 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import EmptyCart from '../cart/EmptyCart';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
 import { useState } from 'react';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -44,6 +45,7 @@ console.log(isValidPhone);
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
   const username = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
@@ -62,6 +64,8 @@ function CreateOrder() {
       <h2 className="mb-8 text-xl font-semibold">
         {"Ready to order? Let's go!"}
       </h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get Position</button>
 
       <Form method="POST" action="/order/new">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
